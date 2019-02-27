fb4fb268866af0c45c9fc93e0249cd6509a01f2e
hlaf <henrique.lindgren@gmail.com>
2019-02-27 20:47:54 +0000

    Allow more general task sources to be used

--- manifests/task.pp
+++ manifests/task.pp
@@ -10,16 +10,22 @@ define razor::task (
   $module    = 'razor',
   $directory = 'tasks',
   $root      = "${::razor::data_root_path}/tasks",
+  $source    = undef,
 ) {
   # Validation
   validate_absolute_path($root)
+
+  $source_ = $source ? {
+    undef   => "puppet:///modules/${module}/${directory}/${name}.task",
+    default => $source,
+  }
 
   # Create directory
   Package[$::razor::server_package_name]
   ->
   file { "${root}/${name}.task":
     ensure  => 'directory',
-    source  => "puppet:///modules/${module}/${directory}/${name}.task",
+    source  => $source_,
     recurse => true,
   }
 }
