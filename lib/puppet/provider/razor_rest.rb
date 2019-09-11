2466356b0a4c2ce3b77979cfba8cf5635dc93e8b
Victor Cabezas <vcabezas@tuenti.com>
2019-09-11 12:47:10 +0200

    Improve error handling in retries

--- lib/puppet/provider/razor_rest.rb
+++ lib/puppet/provider/razor_rest.rb
@@ -103,7 +103,7 @@ class Puppet::Provider::Rest < Puppet::Provider
     response = nil
     begin
       with_retries(:max_tries => @max_retries, :handler => failed_rest_handler) do |attempt_number|
-        Puppet.debug "Trying to concat Razor Server, try #{attempt_number} out of #{@max_retries}"
+        Puppet.debug "Trying to contact Razor Server, try #{attempt_number} out of #{@max_retries}"
         response = RestClient.get url
       end
     rescue => e
