801e1a6b5f22dc839f44d510ad9c7a640d69414b
Victor Cabezas <vcabezas@tuenti.com>
2019-09-16 12:14:59 +0200

    Cleanup microkernel file

--- manifests/microkernel.pp
+++ manifests/microkernel.pp
@@ -27,7 +27,7 @@ class razor::microkernel inherits razor {
     checksum        => $::razor::microkernel_checksum,
     checksum_type   => $::razor::microkernel_checksum_type,
     creates         => "${::razor::repo_store_path}/microkernel",
-    cleanup         => false,
+    cleanup         => true,
     # archive no longer supports a timeout value. Note that the microkernel is about 160 MB.
   }
 }
